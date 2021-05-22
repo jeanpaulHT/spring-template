b7e3e12c82d4355fa992d9317a8a67a33512bd4a
jean.huby <50341023+jeanpaulHT@users.noreply.github.com>
2021-05-22 11:16:13 -0500

    Add files via upload

---
+++ src/test/java/kito/pcm/PcmApplicationTests.java
@@ -0,0 +1,13 @@
+package kito.pcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
